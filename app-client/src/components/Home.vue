e434015010c1eeffd193678fa2134ac746203e12
Nahelou <nahelouquentin@gmail.com>
2021-01-26 11:57:46 +0100

    add dataviz models

---
+++ app-client/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name : "Home",
+    data: () => ({
+
+    }),
+}
+</script>
+
+<style>
+
+</style>
