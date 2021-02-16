31d69e667d82f93e2c13ece265614c00fe81629e
Nahelou <nahelouquentin@gmail.com>
2021-02-16 11:20:48 +0100

    refactor and add canvas container

--- app-client/src/views/Bubble.vue
+++ app-client/src/views/Bubble.vue
@@ -1,29 +1,29 @@
 <template>
-    <v-container>
-      <h1 class="font-weight-light mb-10" >Bubble Plot</h1>
-          <v-select :items="Object.keys(featuresFields)" label="x value" @change="getXValue" >
-          </v-select>
-          <v-select :items="Object.keys(featuresFields)" label="y value" @change="getYValue" >
-          </v-select>
-          <v-select :items="Object.keys(featuresFields)" label="Label" @change="getLabelValue" >
-          </v-select>
-          <v-select :items="Object.keys(featuresFields)" label="Size" @change="getSizeValue" >
-          </v-select>
-          <div v-if="!isPackedData">
-             <label for="colorpicker" class="mb-5" >Color :</label>
-             <input type="color" id="colorpicker" value="#0066ff" @change="changeColor" >
-          </div>
-                 <v-checkbox
-      label="Color based on a field" 
-      @change="isPacked"></v-checkbox>
-          <v-select v-if="isPackedData" :items="Object.keys(featuresFields)" label="Color field" @change="setPackingField" >
-          </v-select>
-          
-        <v-btn @click="processDataviz" right color="success" class="mb-10">
-            Process
-       </v-btn>
-       <canvas id="bubbleChart"></canvas>
-    </v-container>
+  <v-container>
+    <h1 class="font-weight-light mb-10">Bubble Plot</h1>
+    <v-select :items="Object.keys(featuresFields)" label="x value" @change="getXValue">
+    </v-select>
+    <v-select :items="Object.keys(featuresFields)" label="y value" @change="getYValue">
+    </v-select>
+    <v-select :items="Object.keys(featuresFields)" label="Label" @change="getLabelValue">
+    </v-select>
+    <v-select :items="Object.keys(featuresFields)" label="Size" @change="getSizeValue">
+    </v-select>
+    <div v-if="!isPackedData">
+      <label for="colorpicker" class="mb-5">Color :</label>
+      <input type="color" id="colorpicker" value="#0066ff" @change="changeColor">
+    </div>
+    <v-checkbox label="Color based on a field" @change="isPacked"></v-checkbox>
+    <v-select v-if="isPackedData" :items="Object.keys(featuresFields)" label="Color field" @change="setPackingField">
+    </v-select>
+
+    <v-btn @click="processDataviz" right color="success" class="mb-10">
+      Process
+    </v-btn>
+    <div id="graphCanvas">
+      <canvas id="bubbleChart"></canvas>
+    </div>
+  </v-container>
 </template>
 
 <script>
@@ -44,23 +44,22 @@ export default {
   bubbleChartData: null,
   data() {
     return {
-        dataArray: this.$props.dataParsed,
-        featuresFields: this.$props.fields,
-        fieldX: null,
-        fieldY: null,
-        fieldLabel: null,
-        fieldSize: null,
-        color: "rgba(0, 102, 255,.5)",
-        isPackedData : false,
-        packingField: null,
-        };
+      dataArray: this.$props.dataParsed,
+      featuresFields: this.$props.fields,
+      fieldX: null,
+      fieldY: null,
+      fieldLabel: null,
+      fieldSize: null,
+      color: "rgba(0, 102, 255,.5)",
+      isPackedData: false,
+      packingField: null,
+    };
   },
   created() {
     this.dataArray = this.$props.dataParsed;
-    this.featuresFields= this.$props.fields;
+    this.featuresFields = this.$props.fields;
   },
-  mounted() {
-  },
+  mounted() {},
   methods: {
     createChart(chartId, chartData) {
       const ctx = document.getElementById(chartId);
@@ -70,214 +69,191 @@ export default {
         options: chartData.options,
       });
     },
-    getChartData(){
-      // let data = [];
-      // let dataToScale = [];
+    getChartData() {
       let that = this;
-      // for(let i = 0; i < this.dataArray.length; i++){
-      //     let dataOthers = {};
-      //     dataOthers.y = this.dataArray[i][this.fieldY];
-      //     dataOthers.x = this.dataArray[i][this.fieldX];
-      //     dataOthers.label = this.dataArray[i][this.fieldLabel];
-      //     dataOthers.pass = this.dataArray[i][this.fieldSize];
-      //     dataToScale.push(this.dataArray[i][this.fieldSize]);
-      //     data.push(dataOthers);
-      // }
-      // let maxOut = d3.max(Object.values(dataToScale));
-      // let meanOut = d3.mean(Object.values(dataToScale));
-      // let minOut = d3.min(Object.values(dataToScale));
-      // let valued3 = d3
-      //     .scaleLinear(10)
-      //     .domain([minOut, meanOut, maxOut])
-      //     .range([5, 10, 20, 50]);
-      // for(let j=0; j<data.length;j++){
-      //   data[j].r = valued3(data[j].pass);
-      // }
-      // this.dataOthers = data;
+
       let d = that.getDatasetsForChart();
       this.bubbleChartData = {
-      type: "bubble",
-      data: {
-        datasets: d,
-      },
+        type: "bubble",
+        data: {
+          datasets: d,
+        },
         options: {
           legend: {
             labels: {
-                fontColor: 'black'
-            }},
+              fontColor: 'black'
+            }
+          },
           responsive: true,
           lineTension: 1,
           scales: {
-            yAxes: [
-              {
-                scaleLabel : {
-                  display: true,
-                  labelString: this.fieldY,
-                  fontColor: 'black'
-                },
-                ticks: {
-                  beginAtZero: false,
-                  padding: 25,
-                  fontColor: 'black'
-                },
+            yAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: this.fieldY,
+                fontColor: 'black'
               },
-            ],
-            xAxes: [
-              {
-                scaleLabel :{
-                  display: true,
-                  labelString: this.fieldX,
-                  fontColor: 'black'
-                },
-                ticks: {
-                  fontColor: 'black'
-                },
+              ticks: {
+                beginAtZero: false,
+                padding: 25,
+                fontColor: 'black'
               },
-            ],
+            }, ],
+            xAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: this.fieldX,
+                fontColor: 'black'
+              },
+              ticks: {
+                fontColor: 'black'
+              },
+            }, ],
           },
           tooltips: {
             callbacks: {
-                label: function(tooltipItem, data) {
-                  let test = null;
-                  data.datasets[tooltipItem.datasetIndex].data.forEach((e)=>{
-                    if(e.x == parseFloat(tooltipItem.xLabel) && e.y == parseFloat(tooltipItem.yLabel)){
-                      test = e;
-                    }
-                  });
-                    var label = test.label || ' : <br>' + test.x + ' : ' + test.pass;
-                    if (label) {
-                        label += ': ';
-                    }
-                    label += that.fieldX + " : " + test.x;
-                    label += ";" + that.fieldY + " : " + test.y;
-                    return label;
-                },
+              label: function (tooltipItem, data) {
+                let test = null;
+                data.datasets[tooltipItem.datasetIndex].data.forEach((e) => {
+                  if (e.x == parseFloat(tooltipItem.xLabel) && e.y == parseFloat(tooltipItem.yLabel)) {
+                    test = e;
+                  }
+                });
+                var label = test.label || ' : <br>' + test.x + ' : ' + test.pass;
+                if (label) {
+                  label += ': ';
+                }
+                label += that.fieldX + " : " + test.x;
+                label += ";" + that.fieldY + " : " + test.y;
+                return label;
+              },
             },
             enabled: false,
-            custom: function(tooltipModel) {
-                var tooltipEl = document.getElementById('chartjs-tooltip');
-                if (!tooltipEl) {
-                    tooltipEl = document.createElement('div');
-                    tooltipEl.id = 'chartjs-tooltip';
-                    tooltipEl.innerHTML = '<table></table>';
-                    document.body.appendChild(tooltipEl);
-                }
-                if (tooltipModel.opacity === 0) {
-                    tooltipEl.style.opacity = 0;
-                    return;
-                }
-                tooltipEl.classList.remove('above', 'below', 'no-transform');
-                if (tooltipModel.yAlign) {
-                    tooltipEl.classList.add(tooltipModel.yAlign);
-                } else {
-                    tooltipEl.classList.add('no-transform');
-                }
-                function getBody(bodyItem) {
-                    return bodyItem.lines;
-                }
-                if (tooltipModel.body) {
-                    var titleLines = tooltipModel.title || [];
-                    var bodyLines = tooltipModel.body.map(getBody);
-                    var innerHtml = '<thead>';
-                    titleLines.forEach(function(title) {
-                        innerHtml += '<tr><th>' + title + '</th></tr>';
-                    });
-                    innerHtml += '</thead><tbody>';
-                    bodyLines.forEach(function(body, i) {
-                        var colors = tooltipModel.labelColors[i];
-                        var style = 'background:' + colors.backgroundColor;
-                        style += '; border-color:' + colors.borderColor;
-                        style += '; border-width: 2px';
-                        var span = '<span style="' + style + '"></span>';
-                        innerHtml += '<tr><td>' + span + body + '</td></tr>';
-                    });
-                    innerHtml += '</tbody>';
-                    var tableRoot = tooltipEl.querySelector('table');
-                    tableRoot.innerHTML = innerHtml;
-                }
-                var position = this._chart.canvas.getBoundingClientRect();
-                tooltipEl.style.opacity = 1;
-                tooltipEl.style.backgroundColor = "white";
-                tooltipEl.style.position = 'absolute';
-                tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
-                tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
-                tooltipEl.style.fontFamily = tooltipModel._bodyFontFamily;
-                tooltipEl.style.fontSize = tooltipModel.bodyFontSize + 'px';
-                tooltipEl.style.fontStyle = tooltipModel._bodyFontStyle;
-                tooltipEl.style.padding = tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px';
-                tooltipEl.style.pointerEvents = 'none';
+            custom: function (tooltipModel) {
+              var tooltipEl = document.getElementById('chartjs-tooltip');
+              if (!tooltipEl) {
+                tooltipEl = document.createElement('div');
+                tooltipEl.id = 'chartjs-tooltip';
+                tooltipEl.innerHTML = '<table></table>';
+                document.body.appendChild(tooltipEl);
+              }
+              if (tooltipModel.opacity === 0) {
+                tooltipEl.style.opacity = 0;
+                return;
+              }
+              tooltipEl.classList.remove('above', 'below', 'no-transform');
+              if (tooltipModel.yAlign) {
+                tooltipEl.classList.add(tooltipModel.yAlign);
+              } else {
+                tooltipEl.classList.add('no-transform');
+              }
+
+              function getBody(bodyItem) {
+                return bodyItem.lines;
+              }
+              if (tooltipModel.body) {
+                var titleLines = tooltipModel.title || [];
+                var bodyLines = tooltipModel.body.map(getBody);
+                var innerHtml = '<thead>';
+                titleLines.forEach(function (title) {
+                  innerHtml += '<tr><th>' + title + '</th></tr>';
+                });
+                innerHtml += '</thead><tbody>';
+                bodyLines.forEach(function (body, i) {
+                  var colors = tooltipModel.labelColors[i];
+                  var style = 'background:' + colors.backgroundColor;
+                  style += '; border-color:' + colors.borderColor;
+                  style += '; border-width: 2px';
+                  var span = '<span style="' + style + '"></span>';
+                  innerHtml += '<tr><td>' + span + body + '</td></tr>';
+                });
+                innerHtml += '</tbody>';
+                var tableRoot = tooltipEl.querySelector('table');
+                tableRoot.innerHTML = innerHtml;
+              }
+              var position = this._chart.canvas.getBoundingClientRect();
+              tooltipEl.style.opacity = 1;
+              tooltipEl.style.backgroundColor = "white";
+              tooltipEl.style.position = 'absolute';
+              tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
+              tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
+              tooltipEl.style.fontFamily = tooltipModel._bodyFontFamily;
+              tooltipEl.style.fontSize = tooltipModel.bodyFontSize + 'px';
+              tooltipEl.style.fontStyle = tooltipModel._bodyFontStyle;
+              tooltipEl.style.padding = tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px';
+              tooltipEl.style.pointerEvents = 'none';
             }
-        },
+          },
         },
       };
     },
-    getXValue(e){
+    getXValue(e) {
       this.fieldX = e;
     },
-    getYValue(e){
+    getYValue(e) {
       this.fieldY = e;
     },
-    getLabelValue(e){
+    getLabelValue(e) {
       this.fieldLabel = e;
     },
-    getSizeValue(e){
+    getSizeValue(e) {
       this.fieldSize = e;
     },
-    getColors(e){
+    getColors(e) {
       this.colors = [];
-      for(let i = 0; i<e.length; i++){
+      for (let i = 0; i < e.length; i++) {
         this.colors.push(this.color)
       }
     },
-    getRandomColors(color, size){
+    getRandomColors(color, size) {
       let colors = [];
-      for(let i = 0; i < size; i++){
+      for (let i = 0; i < size; i++) {
         colors.push(color);
       }
       return colors;
     },
-    hexToRgbA(hex){
+    hexToRgbA(hex) {
       var c;
-      if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
-          c= hex.substring(1).split('');
-          if(c.length== 3){
-              c= [c[0], c[0], c[1], c[1], c[2], c[2]];
-          }
-          c= '0x'+c.join('');
-          return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',0.5)';
+      if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+        c = hex.substring(1).split('');
+        if (c.length == 3) {
+          c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+        }
+        c = '0x' + c.join('');
+        return 'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',0.5)';
       }
       throw new Error('Bad Hex');
     },
-    changeColor(e){
+    changeColor(e) {
       let color = e.target.value || e.srcElement.value;
       this.color = this.hexToRgbA(color);
     },
-    processDataviz(){
+    processDataviz() {
       document.getElementById("bubbleChart").innerHTML = "";
       this.getColors(this.dataArray);
       this.getChartData();
       this.createChart("bubbleChart", this.bubbleChartData);
     },
-    setPackingField(e){
+    setPackingField(e) {
       this.packingField = e;
       this.packingFieldData = new Set();
-      for(let i = 0; i < this.dataArray.length; i++){
-        if(this.dataArray[i][e]){
+      for (let i = 0; i < this.dataArray.length; i++) {
+        if (this.dataArray[i][e]) {
           this.packingFieldData.add(this.dataArray[i][e]);
         }
       }
     },
-    isPacked(){
-      if(this.packingField){
+    isPacked() {
+      if (this.packingField) {
         this.packingField = null;
       }
       this.isPackedData = !this.isPackedData;
     },
-    getDatasetsForChart(){
+    getDatasetsForChart() {
       let datasets = [];
       let dataToScale = [];
-      if(this.packingField == null){
-        for(let i = 0; i < this.dataArray.length; i++){
+      if (this.packingField == null) {
+        for (let i = 0; i < this.dataArray.length; i++) {
           let dataOthers = {};
           dataOthers.y = this.dataArray[i][this.fieldY];
           dataOthers.x = this.dataArray[i][this.fieldX];
@@ -290,47 +266,44 @@ export default {
         let meanOut = d3.mean(Object.values(dataToScale));
         let minOut = d3.min(Object.values(dataToScale));
         let valued3 = d3
-            .scaleLinear(10)
-            .domain([minOut, meanOut, maxOut])
-            .range([5, 10, 20, 50]);
-        for(let j=0; j<datasets.length;j++){
+          .scaleLinear(10)
+          .domain([minOut, meanOut, maxOut])
+          .range([5, 10, 20, 50]);
+        for (let j = 0; j < datasets.length; j++) {
           datasets[j].r = valued3(datasets[j].pass);
         }
         this.dataOthers = datasets;
-        let dataset = [
-        {
-          label:"Dataset",
-          data : datasets,
+        let dataset = [{
+          label: "Dataset",
+          data: datasets,
           backgroundColor: this.colors,
-              borderColor: [],
-              borderWidth: 1,
-          },
-        ];
+          borderColor: [],
+          borderWidth: 1,
+        }, ];
         console.log(dataset);
         return dataset;
-      }
-      else{
+      } else {
         let datasetsPackedToChartProp = [];
-        let datasetPacked =  {};
+        let datasetPacked = {};
         this.packingFieldData.forEach(field => {
           datasetPacked[field] = [];
         })
 
-        for(let i = 0; i < this.dataArray.length; i++){
+        for (let i = 0; i < this.dataArray.length; i++) {
           let dataPacked = {};
           dataPacked.y = this.dataArray[i][this.fieldY];
           dataPacked.x = this.dataArray[i][this.fieldX];
           dataPacked.label = this.dataArray[i][this.fieldLabel];
           dataPacked.pass = this.dataArray[i][this.fieldSize];
           dataToScale.push(this.dataArray[i][this.fieldSize]);
-          if(this.dataArray[i][this.packingField]){
+          if (this.dataArray[i][this.packingField]) {
             datasetPacked[this.dataArray[i][this.packingField]].push(dataPacked);
           }
         }
-        for(let valuePacker in datasetPacked){
+        for (let valuePacker in datasetPacked) {
           let datasetPackedToChartProp = {
-            label : valuePacker,
-            data : datasetPacked[valuePacker],
+            label: valuePacker,
+            data: datasetPacked[valuePacker],
             backgroundColor: this.getRandomColors(this.randomRgbColor(), datasetPacked[valuePacker].length),
             borderColor: [],
             borderWidth: 1,
@@ -341,11 +314,11 @@ export default {
         let meanOut = d3.mean(Object.values(dataToScale));
         let minOut = d3.min(Object.values(dataToScale));
         let valued3 = d3
-            .scaleLinear(10)
-            .domain([minOut, meanOut, maxOut])
-            .range([5, 10, 20, 50]);
-        for(let v in datasetPacked){
-          for(let j=0; j < datasetPacked[v].length;j++){
+          .scaleLinear(10)
+          .domain([minOut, meanOut, maxOut])
+          .range([5, 10, 20, 50]);
+        for (let v in datasetPacked) {
+          for (let j = 0; j < datasetPacked[v].length; j++) {
             datasetPacked[v][j].r = valued3(datasetPacked[v][j].pass);
           }
         }
